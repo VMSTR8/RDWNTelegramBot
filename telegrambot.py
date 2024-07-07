c42a6428f390dca9bc74ec56d82a4cf4bd7a782f
Maxim Vinokurov <vmstr8@yandex.ru>
2024-07-07 16:23:42 +0300

    for testing (in production, of course), I updated the file and added the admin function.

--- telegrambot.py
+++ telegrambot.py
@@ -10,6 +10,7 @@ from telegram.ext import (
 from tortoise import run_async
 
 from telegram_handlers.menu import start
+from telegram_handlers.admin import admin
 from database.init import init
 
 
@@ -25,7 +26,10 @@ class TelegramBot:
 
         start_handler = CommandHandler('start', start)
 
+        admin_test = CommandHandler('admin', admin)
+
         self.application.add_handler(start_handler)
+        self.application.add_handler(admin_test)
 
         logging.info('TelegramBot class worked properly, running polling')
         self.application.run_polling()
